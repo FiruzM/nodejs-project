31fa6790fdaed260eb8138589a0b68e7a57ebe60
Firuz M <madaminovfiruz659@gmail.com>
2025-05-07 21:06:20 +0500

    add new query params

--- controllers/getDataByPathParam.js
+++ controllers/getDataByPathParam.js
@@ -2,11 +2,16 @@ import { products } from "../data/data.js";
 
 export const getDataByPathParamController = (req, res) => {
   const { field, term } = req.params;
-  const allowedFields = ["id"];
+  const allowedFields = ["id", "category"];
   if (!allowedFields.includes(field)) {
     return res.status(400).json({
       message: "Search field not allowed. Please use only by 'category'",
     });
+  }
+
+  if (field === "category") {
+    const filteredData = products.filter((item) => item[field] === term);
+    return res.json(filteredData);
   }
   const filteredData = products.find((item) => item[field] === +term);
 
--- utils/queryData.js
+++ utils/queryData.js
@@ -5,5 +5,31 @@ export const queryData = (data, query) => {
     );
   }
 
+  if (query.category) {
+    data = data.filter((item) => item.category === query.category);
+  }
+
+  if (query.discount === "true") {
+    data = data.filter(
+      (item) => item.discount !== null && item.discount !== undefined
+    );
+  }
+
+  if (query.from) {
+    data = data.filter((item) => item.price >= +query.from);
+  }
+
+  if (query.to) {
+    data = data.filter((item) => item.price <= +query.to);
+  }
+
+  if (query.sort) {
+    if (query.sort === "asc") {
+      data = data.sort((a, b) => a.price - b.price);
+    }
+    if (query.sort === "desc") {
+      data = data.sort((a, b) => b.price - a.price);
+    }
+  }
   return data;
 };
